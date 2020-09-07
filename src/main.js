cf675d9540aad1c4f37b55f6d2c4eccc154238cb
violateer <1828257089@qq.com>
2020-09-07 15:56:01 +0800

    完成添加商品

--- src/main.js
+++ src/main.js
@@ -7,13 +7,16 @@ import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 import axios from 'axios'
 // 配置请求的跟路径
-// axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
-// axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
-// axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
-axios.defaults.baseURL = 'http://47.101.61.134:12345/api/private/v1/'
+axios.defaults.baseURL = 'http://www.violateer.top:12345/api/private/v1/'
 
 axios.interceptors.request.use(config => {
     // console.log(config)
@@ -26,6 +29,23 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+    const dt = new Date(originVal * 1000)
+
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
     router,
